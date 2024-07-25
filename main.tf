4976b1045167faaf99a10abb0d2bd896477ad979
venkatesh186 <arikicherlavenkatesulu@gmail.com>
2024-07-25 19:00:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "dv" { #creating ec2 instance
+resource "aws_instance" "d" { #creating ec2 instance
   ami = "ami-068e0f1a600cd311c"
   instance_type = "t2.micro"
   key_name = "dibakar"
